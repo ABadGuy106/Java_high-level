0f2c87ffbe80006cf8034a307602f2d558a70d97
zhangsan <zhangsan@163.com>
2020-07-03 14:43:12 +0800

    创建项目

---
+++ java-high-level-api/thread-api/src/main/java/net/abadguy/thread/TestThread.java
@@ -0,0 +1,17 @@
+package net.abadguy.thread;
+
+/**
+ * 多线程的创建
+ *      方式一：继承Thread类
+ */
+public class TestThread {
+}
+
+
+
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        super.run();
+    }
+}
